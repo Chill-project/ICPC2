dac42c1762ac197daddd556269a60c749eeff023
Julien Fastré <julien.fastre@champs-libres.coop>
2015-10-27 22:19:22 +0100

    bootstrap phpunit and embedded application

---
+++ Test/Fixtures/App/app/AppKernel.php
@@ -0,0 +1,46 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Chill\CustomFieldsBundle\ChillCustomFieldsBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Chill\PersonBundle\ChillPersonBundle(),
+            new Chill\MainBundle\ChillMainBundle(),
+            new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            #add here all the required bundle (some bundle are not required)
+        );
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return sys_get_temp_dir().'/ICPC2Bundle/cache';
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogDir()
+    {
+        return sys_get_temp_dir().'/ICPC2Bundle/logs';
+    }
+}
